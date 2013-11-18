5247ac49d994b5c13bfbe39208e1209ea0546642
Ander Rodriguez <AnderRodriguezVarela@gmail.com>
2013-11-18 19:46:05 +0100

    Added karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -46,11 +46,19 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
+
+    // code coverage (karma-coverage)
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      'app/scripts/*.js': ['coverage'],
+      'app/scripts/**/*.js': ['coverage']
+    },
+
   });
 };

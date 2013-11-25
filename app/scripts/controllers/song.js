bc7c95f8005dbb895aebbe5e22b57dceab9f6334
Ander Rodriguez <AnderRodriguezVarela@gmail.com>
2013-11-25 22:23:44 +0100

    Add some general styles

--- app/scripts/controllers/song.js
+++ app/scripts/controllers/song.js
@@ -2,11 +2,16 @@
 
 angular.module('fillInApp')
   .controller('SongCtrl', function ($scope, $routeParams, $http, $location, Songs) {
-
-    Songs.get({songId: $routeParams.itemId}, function (songs) {
-      $scope.song = songs;
-      $scope.$root.pageTitle = $scope.song.name;
-    });
+    Songs.get(
+      {
+        songId: $routeParams.itemId
+      }, function (song) {
+        $scope.song = song;
+        $scope.$root.pageTitle = $scope.song.name;
+      }, function () {
+        // TODO handle error
+        $location.path('/songlist');
+      });
 
     $scope.correct = function ($event) {
       $event.preventDefault();
@@ -24,17 +29,17 @@ angular.module('fillInApp')
       inputs.each(function() {
         input = $(this);
         if(input.val().toLowerCase() === input.data('result').toString().toLowerCase()) {
-          input.css({'border-color': '#468847', 'color': '#468847'});
+          input.removeClass('error').addClass('correct');
           inputCorrect += 1;
         } else {
-          input.css({'border-color': '#b94a48', 'color': '#b94a48'});
+          input.removeClass('correct').addClass('error');
           correct = false;
         }
       });
 
-      result.html(inputCorrect+'/'+inputCount).fadeIn();
+      result.html(inputCorrect + '/' + inputCount).fadeIn();
       if(correct) {
-        result.css({'border-color': '#468847', 'color': '#468847'});
+        result.addClass('correct');
         // TODO
         console.log('CORRECT!');
       }
@@ -44,11 +49,13 @@ angular.module('fillInApp')
 
     $scope.save = function () {
       $scope.song.$save(function (song) {
-        $location.path('/song/'+song._id);
+        $location.path('/song/' + song._id);
       });
     };
 
     $scope.delete = function () {
-      $scope.song.$delete();
+      $scope.song.$delete(function () {
+        $location.path('/songlist');
+      });
     };
   });
